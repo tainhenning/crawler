54be96daa20a7461f81fc2d414e5c8daca87d898
Tain Henning <tain.henning@gmail.com>
2018-02-18 14:40:07 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def trade_spider(max_pages):
+    page = 0
+    while page <= max_pages:
+        url = 'https://majestic.com/reports/majestic-million?s=' + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = bs(plain_text)
+        for link in soup.findAll('a', {'rel':'nofollow'}):
+            href = link.get('href')
+        page += 100
+trade_spider(999900)
